a36e7957aa5a1f079e983c18fff5602104abc74f
rishab <kumarrishab451@gmail.com>
2024-10-11 11:26:19 +0530

    changes were made and added to the remote repo

--- src/App.js
+++ src/App.js
@@ -1,24 +1,40 @@
-import logo from './logo.svg';
-import './App.css';
 
+import './App.css';
+import { Hero } from './Components/Hero/Hero';
+// import { Hero } from './Components/Hero/Hero';
+import { Navbar } from './Components/Navbar/Navbar';
+import { Cart } from './Pages/Cart';
+import { LoginSignup } from './Pages/LoginSignup';
+import { Product } from './Pages/Product';
+import { Shop } from './Pages/Shop';
+import { ShopCategory } from './Pages/ShopCategory';
+import { BrowserRouter,Routes,Route } from 'react-router-dom';
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+   
+    <div>
+       <BrowserRouter>
+           <Navbar/>
+      <Routes>
+        <Route path='/' element={<Shop/>}/>
+        <Route path='/mens' element={<ShopCategory category="men"/>}/>
+        <Route path='/womens' element={<ShopCategory category="women"/>}/>
+        <Route path='/kids' element={<ShopCategory category="kid"/>}/>
+        <Route path='/product' element={<Product/>}/>
+        <Route path=':ProductId' element={<Product/>}/>
+        <Route path='/cart' element={<Cart/>}/>
+        <Route path='/login' element={<LoginSignup/>}/>
+      </Routes>
+       </BrowserRouter>
+
+       <Hero/>
+     
+      
+      
+      
+      
     </div>
+    
   );
 }
 
